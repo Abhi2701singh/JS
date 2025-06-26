04b4617829e74ac1b54fc50ed1aa92e027e133c9
ABHINAV SINGH <abhinavsi434@gmail.com>
2025-06-26 23:45:08 +0530

    commit

---
+++ Javascript Fundamentals/conditional_statement.js
@@ -0,0 +1,26 @@
+/** conditional statement **/
+
+// if(condition/expression){
+//     // code to be executed if the condition is true
+// }
+
+
+// if (5 > 3) {
+//     console.log("5 is greater than 3");
+// }
+
+
+// if(true){
+//     console.log("This condition is true");
+    
+// }
+
+
+// if (false) {
+//     console.log("This condition is false");
+// } else {
+//     console.log("This condition is false, so this block is executed");
+// }
+
+
+
