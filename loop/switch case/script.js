0e02a65e0fe5f2e85ffef0466429c9ac1b5afb03
ABHINAV SINGH <abhinavsi434@gmail.com>
2025-07-15 21:16:12 +0530

    commit

---
+++ loop/switch case/script.js
@@ -0,0 +1,37 @@
+var i=9;
+
+switch(i){
+
+    case 1:
+    document.write("i is equal to 1");
+    break;
+    case 2:
+    document.write("i is equal to 2");
+    break;
+    case 3:
+    document.write("i is equal to 3");
+    break;
+    case 4:
+    document.write("i is equal to 4");
+    break;
+    case 5:
+    document.write("i is equal to 5");
+    break;
+    case 6:
+    document.write("i is equal to 6");
+    break;
+    case 7:
+    document.write("i is equal to 7");
+    break;
+    case 8:
+    document.write("i is equal to 8");
+    break;
+    case 9:
+    document.write("i is equal to 9");
+    break;
+    case 10:
+    document.write("i is equal to 10");
+
+    default:
+        document.write("i is greater than 9");
+}
